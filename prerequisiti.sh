55df45957b5297117936895fb5589614ad1df75a
MontyDLemon <81575843+MontyDLemon@users.noreply.github.com>
2021-11-13 10:56:08 +0100

    Update prerequisiti.sh

--- prerequisiti.sh
+++ prerequisiti.sh
@@ -5,13 +5,13 @@ sudo apt install -y apache2 openssh-client openssh-server lvm2
 # configuro iptables
 
 #regole in ingresso
-iptables -A INPUT -i eth1 -p udp -m multiport --dports 5404,5405,5406 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+sudo iptables -A INPUT -i eth1 -p udp -m multiport --dports 5404,5405,5406 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
 #regole in uscita
-iptables -A OUTPUT -o eth1 -p udp -m multiport --dports 5404,5405,5406 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+sudo iptables -A OUTPUT -o eth1 -p udp -m multiport --dports 5404,5405,5406 -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
 #salvo regole
-iptables -L -n
+sudo iptables -L -n
 
 #salvo su file chiamato rules.v4
-iptables -save > /etc/iptables/rules.v4
+sudo iptables -save > /tmp/rules.v4
