80dc4a9766fc8ce50c8500689c98cf29d4dbbefa
MontyDLemon <81575843+MontyDLemon@users.noreply.github.com>
2021-11-20 12:38:38 +0100

    Update condorJobStress.sh

--- condorJobStress.sh
+++ condorJobStress.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 #tramite il pacchetto stress creo un job per superare il 60% di cpu
-stress --cpu 1 --timeout 15
+
+#stress --cpu 1 --timeout 15
+
+#uso invece il comando yes per aumentare il carico sulla cpu
+#il comando yes stampa y all'infinito, per evitare l'output a schermo lo rimando su /dev/null (cestino)
+
+yes > /dev/null
