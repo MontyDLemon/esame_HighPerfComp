2825f5c3f1373dd7414974087472b6a9e38cc2ea
MontyDLemon <81575843+MontyDLemon@users.noreply.github.com>
2021-12-06 10:49:17 +0100

    Update staticIp.sh

--- PrimaEsperienza_HighAvCluster/Debian_Configuration/staticIp.sh
+++ PrimaEsperienza_HighAvCluster/Debian_Configuration/staticIp.sh
@@ -1,6 +1,6 @@
 #aggiungere IP statico all'interfaccia di rete
 
-#nel file sudo nano /etc/network/interfaces, scrivere il seguente codice
+#nel file sudo nano /etc/network/interfaces, modificare in basso e sostituire con il seguente codice
 
 # The primary network interface
 allow-hotplug eth0
@@ -8,3 +8,5 @@ iface enp0s3 inet static
         address 10.0.2.4
         netmask 255.255.255.0
         gateway 10.0.2.1
+
+#riavviare macchina, verificare con ip a
